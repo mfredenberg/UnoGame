5c4fb3963a43d6d6161531c5e3e81d974252c8e7
mfredenberg <fredenbe20@up.edu>
2018-03-27 20:29:26 -0700

    Added classes

---
+++ app/src/main/java/com/example/fredenbe20/uno/Uno/UnoComputerPlayer.java
@@ -0,0 +1,19 @@
+package com.example.fredenbe20.uno.Uno;
+
+import com.example.fredenbe20.uno.game.GameComputerPlayer;
+import com.example.fredenbe20.uno.game.infoMsg.GameInfo;
+
+/**
+ * Created by fredenbe20 on 3/27/2018.
+ */
+
+public class UnoComputerPlayer extends GameComputerPlayer {
+
+    public UnoComputerPlayer(String name){
+        super(name);
+    }
+    @Override
+    protected void receiveInfo(GameInfo info) {
+
+    }
+}
---
+++ app/src/main/java/com/example/fredenbe20/uno/Uno/actionMsg/PlaceCardAction.java
@@ -0,0 +1,8 @@
+package com.example.fredenbe20.uno.Uno.actionMsg;
+
+/**
+ * Created by fredenbe20 on 3/27/2018.
+ */
+
+public class PlaceCardAction {
+}
