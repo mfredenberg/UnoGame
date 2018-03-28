a4db7e16a330ca098321b461c65ac40713c96d86
papoutsa20 <35584110+papoutsa20@users.noreply.github.com>
2018-03-27 19:46:45 -0700

    a

---
+++ app/src/main/java/com/example/fredenbe20/uno/Uno/Type.java
@@ -0,0 +1,24 @@
+package com.example.fredenbe20.uno.Uno;
+
+/**
+ * Created by steli on 3/27/2018.
+ */
+
+public enum Type {
+    ZERO,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    SKIP,
+    PLUS2,
+    WILD,
+    REVERSE,
+    WILDDRAW4;
+
+}
